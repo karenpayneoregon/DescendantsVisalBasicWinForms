65cd14bbf43dd6fa3cad15fc823133d6ba369846
Karen Payne <paynekaren@comcast.net>
2019-09-01 03:55:39 -0700

    Added example which shows how to get only visible label and text box controls to a concatenated string.

---
+++ ExampleInvisibleControls/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GetVisibleControls = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(18, 145)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Label5"
+        Me.Label5.Visible = False
+        '
+        'GetVisibleControls
+        '
+        Me.GetVisibleControls.Location = New System.Drawing.Point(12, 195)
+        Me.GetVisibleControls.Name = "GetVisibleControls"
+        Me.GetVisibleControls.Size = New System.Drawing.Size(177, 23)
+        Me.GetVisibleControls.TabIndex = 5
+        Me.GetVisibleControls.Text = "Get visible labels and text boxes"
+        Me.GetVisibleControls.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(63, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = "Blue"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(63, 57)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 7
+        Me.TextBox2.Text = "Karen"
+        Me.TextBox2.Visible = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(63, 83)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
+        Me.TextBox3.Text = "Orange"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(63, 112)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 9
+        Me.TextBox4.Text = "Payne"
+        Me.TextBox4.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(63, 138)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 10
+        Me.TextBox5.Text = "Red"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(261, 230)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GetVisibleControls)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Get only visible"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GetVisibleControls As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+End Class
---
+++ ExampleInvisibleControls/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Text
+Imports DescendantsLibrary
+
+Public Class Form1
+    ''' <summary>
+    ''' Get visible Label and TextBox controls. Note that the controls
+    ''' are not ordered but can be via .OrderBy Lambda extension.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub GetVisibleControls_Click(sender As Object, e As EventArgs) Handles GetVisibleControls.Click
+        Dim visibleTextBoxes = TextBoxVisibleList()
+
+        If visibleTextBoxes.Count > 0 Then
+            Console.WriteLine(String.Join(",", visibleTextBoxes.Select(Function(item) item.Text)))
+        End If
+
+        Dim visibleLabels = LabelVisibleList()
+
+        If visibleLabels.Count > 0 Then
+            Console.WriteLine(String.Join(",", visibleLabels.Select(Function(item) item.Text)))
+        End If
+
+        Dim sb As New StringBuilder
+
+        '
+        ' Basics of working with conditions
+        '
+        If visibleTextBoxes.Count > 0 AndAlso visibleLabels.Count > 0 Then
+            sb.Append(String.Join(",", visibleTextBoxes.Select(Function(item) item.Text)) & ",")
+            sb.Append(String.Join(",", visibleLabels.Select(Function(item) item.Text)))
+        End If
+
+        If sb.Length > 0 Then
+            MessageBox.Show(sb.ToString())
+        Else
+            MessageBox.Show("Nothing to show")
+        End If
+
+        '
+        ' No assertion, just combine text for each visible control
+        '
+        Dim textBoxesTextOnly = visibleTextBoxes.Select(Function(item) item.Text).ToArray()
+        Dim labelsTextOnly = visibleLabels.Select(Function(item) item.Text).ToArray()
+        Dim combined = textBoxesTextOnly.Concat(labelsTextOnly).ToArray()
+        Dim results = String.Join(",", combined)
+
+        MessageBox.Show(results)
+
+    End Sub
+End Class
+Public Module Extensions
+    <Runtime.CompilerServices.Extension>
+    Public Function TextBoxVisibleList(pControl As Control) As List(Of TextBox)
+        Return pControl.TextBoxList().Where(Function(box) box.Visible).ToList()
+    End Function
+    ''' <summary>
+    ''' Get list of Label on control
+    ''' </summary>
+    ''' <param name="pControl">Control to find controls on</param>
+    ''' <returns>List of Label or empty list</returns>
+    <Runtime.CompilerServices.Extension>
+    Public Function LabelVisibleList(pControl As Control) As List(Of Label)
+        Return pControl.LabelList().Where(Function(lb) lb.Visible).ToList()
+    End Function
+End Module
